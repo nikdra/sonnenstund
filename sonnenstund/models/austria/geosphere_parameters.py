92dee5784239f86e0dec2f4ccf54fe65b5f58163
Nikola <nikoladragovic@hotmail.com>
2026-04-18 01:03:20 +0200

    feat: more data exploration

---
+++ sonnenstund/models/austria/geosphere_parameters.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+from sonnenstund.models.quality import Quality
+
+class _GeoSphereAustriaParameter(BaseModel):
+    """Class for describing the shape of the parameter response from GeoSphere Austria
+
+    Args:
+        BaseModel (_type_): pydantic Base Model
+    """
+    name: str
+    unit: str
+    data: list[int | float]
+
+
+class _GeoSphereAustriaSunHoursParameter(_GeoSphereAustriaParameter):
+    @property
+    def sun_minutes(self):
+        return [h * 60 for h in self.data] # for the future (other sources?) it might be good to standardize early
+    
+
+class _GeoSphereAustriaSunHoursQualityParameter(_GeoSphereAustriaParameter):
+    def _quality_conversion(self, val: int | float):
+        if val < 10:
+            return Quality.LOW
+        if 10 < val < 20:
+            return Quality.MEDIUM
+        if 20 < val:
+            return Quality.HIGH
+        return Quality.UNKNOWN # unknown quality for this source; can be null
+    
+    @property
+    def quality(self):
+        return [self._quality_conversion(v) for v in self.data] # for the future (other sources?) it might be good to standardize early
+    
+
+
+class GeoSphereAustriaParameters(BaseModel):
+    """Class for defining the parameters of a geopandas request from GeoSphere Austria
+
+    Args:
+        BaseModel (_type_): pydantic Base Model
+    """
+    so_h: _GeoSphereAustriaSunHoursParameter
+    so_h_flag: _GeoSphereAustriaSunHoursQualityParameter
+
+
---
+++ sonnenstund/models/quality.py
@@ -0,0 +1,7 @@
+from enum import StrEnum, auto
+
+class Quality(StrEnum):
+    HIGH = auto()
+    MEDIUM = auto()
+    LOW = auto()
+    UNKNOWN = auto()
